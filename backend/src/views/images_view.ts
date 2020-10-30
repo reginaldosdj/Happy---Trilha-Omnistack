362cf0000dbc62dd7b6166e6b39944af2ce7a1e1
Reginaldo <reginaldosdj@yahoo.com>
2020-10-30 06:18:51 -0300

    TRILHA FINALIZADA

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -4,11 +4,12 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://localhost:3333/uploads/${image.path}`
-    }
+      url: `http://192.168.0.101:3333/uploads/${image.path}`,
+    };
   },
 
   renderMany(images: Image[]) {
     return images.map(image => this.render(image));
   }
 }
+
